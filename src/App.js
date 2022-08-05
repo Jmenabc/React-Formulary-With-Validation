4102549511e1fb4ef284f8e14a5eca762cb46f4a
javi mena <menajavier2002@gmail.com>
2022-08-05 10:47:21 +0200

    funciones de validacion  y estados creados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Formulario, Label, ContenedorTerminos, ContenedorBotonCentrado, Boton, MensajeExito, MensajeError } from './elementos/Formularios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
 import ComponenteInput from './componentes/Input';
 
 const App = () => {
+  const [usuario, cambiarUsuario] = useState({ campo: '', valido: null });
+  const [nombre, cambiarNombre] = useState({ campo: '', valido: null });
+  const [password, cambiarPassword] = useState({ campo: '', valido: null });
+  const [password2, cambiarPassword2] = useState({ campo: '', valido: null });
+  const [correo, cambiarCorreo] = useState({ campo: '', valido: null });
+  const [telefono, cambiarTelefono] = useState({ campo: '', valido: null });
+
+  const expresiones = {
+    usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+    nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+    password: /^.{4,12}$/, // 4 a 12 digitos.
+    correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+  }
+  
   return (
     <main>
       <Formulario action=''>
+        
         <ComponenteInput
+          estado={usuario}
+          cambiarEstado={cambiarUsuario}
           type="text"
           label="Usuario"
           placeholder="Usuario"
--- src/componentes/Input.js
+++ src/componentes/Input.js
@@ -2,12 +2,34 @@ import React from "react";
 import { Input, Label, GrupoInput, LeyendaError, IconoValidacion } from './../elementos/Formularios';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
-const ComponenteInput = ({ label, placeholder, type, name, errorLeyend, regularExpresion }) => {
+const ComponenteInput = ({ estado, cambiarEstado, label, placeholder, type, name, errorLeyend, regularExpresion }) => {
+     const myOnChange = (e) => {
+         cambiarEstado({...estado, campo: e.target.value});
+     }
+
+     const validacion = () => {
+         if(regularExpresion) {
+             if(regularExpresion.test(estado.campo)) {
+                 console.log('input correcto')
+             } else {
+                 console.log('input no valido')
+             }
+         }
+     }
+    
     return (
         <div>
             <Label htmlFor={name}>{label}</Label>
             <GrupoInput>
-                <Input type={type} placeholder={placeholder} id={name} />
+                <Input
+                    type={type}
+                    placeholder={placeholder}
+                    id={name}
+                    // value={estado.campo}
+                     onChange={myOnChange}
+                     onKeyUp={validacion}
+                     onBlur={validacion}
+                />
                 <IconoValidacion icon={faCheckCircle} />
             </GrupoInput>
             <LeyendaError>{errorLeyend}</LeyendaError>
